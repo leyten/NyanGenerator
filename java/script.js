4d8f860ed47b1b50beeb62b897fee11871051d6c
leyten <86632281+leyten@users.noreply.github.com>
2024-05-06 01:22:08 +0200

    Add files via upload

---
+++ java/script.js
@@ -0,0 +1,12 @@
+// This is just a placeholder. You'll need to implement these features yourself.
+document.getElementById('reset').addEventListener('click', function() {
+    console.log('Reset GIF');
+});
+
+document.getElementById('random').addEventListener('click', function() {
+    console.log('Generate Random GIF');
+});
+
+document.getElementById('download').addEventListener('click', function() {
+    console.log('Download GIF');
+});
